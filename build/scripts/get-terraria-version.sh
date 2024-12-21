833d9c9b36143d8109438a772329458167e9bd69
Alex Paarfus <stewie410@gmail.com>
2024-12-21 18:22:09 -0500

    Switch to alpine; simplify install/launch procedure; configure with a config file

--- build/scripts/get-terraria-version.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-curl -sfL 'https://terraria.wiki.gg/wiki/Server#Downloads' | \
-    sed -n '/Terraria Server/s/^.*Terraria Server \([0-9\.]*\)<.*$/\1/p' | \
-    sed 1q
---
+++ build/scripts/get_tvs.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+latest() {
+    curl -sfL 'https://terraria.wiki.gg/wiki/Server#Downloads' | \
+        sed -n '/Terraria Server/s/^.*Terraria Server \([0-9\.]*\)<.*$/\1/p' | \
+        sed 1q
+}
+
+unpack() {
+    local zip prefix stage
+    zip="$(mktemp)"
+    prefix="https://terraria.org/api/download/pc-dedicated-server"
+
+    curl -sfo "${zip}" "${prefix}/terraria-server-${TERRARIA_VERSION}.zip" || \
+        return 1
+
+    stage="$(mktemp -d)"
+    unzip "${zip}" -d "${stage}" || return 1
+    find "${stage}/${TERRARIA_VERSION}/Linux" \
+        -mindepth 1 \
+        -exec mv {} "${TERRARIA_DIR}/" \
+        \;
+
+    rm -rf "${zip}" "${stage}"
+
+    return 0
+}
+
+main() {
+    if [[ "${TERRARIA_VERSION:=latest}" == "latest" ]]; then
+        TERRARIA_VERSION="$(latest)"
+        printf 'Using latest Terraria version: %s\n' "${TERRARIA_VERSION}"
+        export TERRARIA_VERSION="${TERRARIA_VERSION//./}"
+    fi
+
+    printf '%s\n' "${TERRARIA_VERSION}" > "${TERRARIA_DIR}/terraria-version.txt"
+    unpack
+}
+
+main
